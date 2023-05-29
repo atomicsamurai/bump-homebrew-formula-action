e9b881564e87d65bb9ed655f89769578eb71d017
Sandeep Chaturvedi <schaturvedi@gmail.com>
2023-05-29 16:17:55 -0600

    Update run.ts

--- src/run.ts
+++ src/run.ts
@@ -4,12 +4,16 @@ import { UpgradeError } from './replace-formula-fields'
 import run from './main'
 
 run(api).catch((error) => {
+  console.log(`sandlog: run 1`)
   if (error instanceof UpgradeError) {
     console.warn('Skipping: %s', error.message)
     return
   }
+  console.log(`sandlog: run 2`)
   setFailed(error.toString())
+  console.log(`sandlog: run 3`)
   if (process.env.GITHUB_ACTIONS == undefined) {
     console.error(error.stack)
   }
+  console.log(`sandlog: run 4`)
 })
